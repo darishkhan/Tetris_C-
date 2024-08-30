d8cae2d012ab3c2fba71ddf6fca5fb7bce094423
Darish Khan <darishkhanon155@gmail.com>
2024-08-30 17:11:39 +0530

    Grid completed!

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,20 @@
 #include<raylib.h>
+#include "grid.h"
 
 int main()
 {
+    Color darkGreen = {1, 50, 32, 255};
     InitWindow(300, 600, "Tetris by Darish");
     SetTargetFPS(60);
+
+    Grid grid = Grid();
+    grid.Print();
+
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
-
+        ClearBackground(darkGreen);
+        grid.Draw();
         EndDrawing();
     }
     CloseWindow();
